44338fe5a5cbae2d4a4b1ff969c92ee0d2f60cd8
Karl-Johan Alm <karljohan-alm@garage.co.jp>
2020-09-21 20:57:00 +0900

    comment out old invalid stuff, and fill in more details on recording

--- wave-reader.c
+++ wave-reader.c
@@ -16,12 +16,12 @@ struct tag_t {
 
 struct fmt_t {
     struct tag_t tag;
-    uint16_t format; // 1 = PCM
-    uint16_t channels; // 1 = mono, 2 = stereo
-    uint32_t sample_rate; // samples per second, e.g. 44100
-    uint32_t bytes_per_sec; // sample rate * block align
-    uint16_t block_align; // channels * bits/sample / 8
-    uint16_t bits_per_sample; // 8 or 16
+    uint16_t format;            // 1 = PCM
+    uint16_t channels;          // 1 = mono, 2 = stereo
+    uint32_t sample_rate;       // samples per second, e.g. 44100
+    uint32_t bytes_per_sec;     // sample rate * block align
+    uint16_t block_align;       // channels * bits/sample / 8
+    uint16_t bits_per_sample;   // 8 or 16
 };
 
 struct fmt_t* fmt_read(FILE* stream) {
@@ -122,9 +122,10 @@ bool playback_iterate(struct playback_t* pb) {
     if (pb->pos + bytes_per_sample * pb->riff->fmt->channels > pb->riff->data->tag.len) return false;
     for (uint8_t channel = 0; channel < pb->riff->fmt->channels; ++channel) {
         if (bytes_per_sample == 1) {
-            int8_t i8;
+            // apparently 1-byte samples are unsigned; we undo that
+            uint8_t i8;
             memcpy(&i8, &pb->riff->data->data[pb->pos++], 1);
-            pb->chn[channel] = i8;
+            pb->chn[channel] = (int16_t)i8 - 127;
         } else {
             memcpy(&pb->chn[channel], &pb->riff->data->data[pb->pos], 2);
             pb->pos += 2;
@@ -176,16 +177,35 @@ bool analyzer_analyze(struct analyzer_t* anl, size_t samples) {
     return true;
 }
 
+const char* sample_timestr(uint32_t sample, uint32_t rate) {
+    static char* rv = NULL;
+    if (!rv) rv = malloc(6);
+    uint32_t hs = sample * 100 / rate;
+    sprintf(rv, "%02u.%02u", hs / 100, hs % 100);
+    return rv;
+}
+
 int main(const int argc, const char* argv[]) {
     /*
      * 32-64 bits of timestamp
      * 16-32 bits of temp (0000..9999)
-     * 15 (days)
-     * = 7680 .. 30720 bits of data
+     * 7 (days)
+     * = 336 .. 672 bits of data
      * transmitted over 29.6 seconds with 44100 sample rate
      * total 1305771 samples; given pauses in between each burst of data, assume 80% was transmission and 20% was gap
      * giving 23.7 seconds of transmission to transmit 7680-30720 bits of data: 0.0007714844 up to 0.003085937 seconds per bit
      * i.e. 40-200 samples per bit
+     *
+     * each transmission burst has ~52000 samples over ~1.2 seconds.
+     * the data is split into 15 bursts. if we assume no repetition of data,
+     * and assume that the data is simply cut into 15 pieces arbitrarily, we get something like this:
+     *        |       |       |       |       |       |       |       |       |       |       |       |       |       |       
+     * [timestamp][temp][timestamp][temp][timestamp][temp][timestamp][temp][timestamp][temp][timestamp][temp][timestamp][temp]
+     * which could be the case e.g. if the data is serialized into a data stream that is abstracted away from the actual
+     * transmission details.
+     * if however each burst is a day, there is some repetition and/or checksumming as we are receiving 2x + 1 days' worth of
+     * data
+     * it is also possible that the data is compressed before being transmitted. that sounds overkill tho.
      */
     if (argc != 2) {
         fprintf(stderr, "syntax: %s <wav file>\n", argv[0]);
@@ -215,59 +235,127 @@ int main(const int argc, const char* argv[]) {
         if (c > max) max = c;
     }
     printf("sample range: %d .. %d\n", min, max);
-    // we define quality of parameters as the set where we saw the highest top count divided by total counts,
-    // i.e. a perfect score for 100 samples is where top sample count = 100
-    // and a 50% score is where the top sample count = 50, so we can calculate the quality
-    // of a single analyze as topcount / count,
-    // and the aggregate score as the sum of all topcounts divided by the sum of counts
-    // our goal is to find a signal with ON and OFF (true/false, 1/0) elements, which means we also want
-    // to raise quality for when there are two distinct signals, where the distinction is defined
-    // by the steps implementation (i.e. a 58 is considered distinctly different from a 59)
-    // finally, the end quality is defined as the top TWO hits across the entire run, and their counts over the entire counts
-    double top_score = 0;
-    #define STEPS 10000
-    struct analyzer_t* anl = analyzer_init(pb, min, max, STEPS);
-    uint64_t tally_counts[STEPS];
-    for (size_t window = 10; window < 500; ++window) {
-        for (size_t slide = 0; slide < window; ++slide) {
-            printf("w = %5zu, slide = %5zu\r", window, slide);
-            fflush(stdout);
-            playback_rewind(pb);
-            // "slide"
-            for (size_t i = 0; i < slide; ++i) playback_iterate(pb);
-            memset(tally_counts, 0, sizeof(uint64_t) * STEPS);
-            uint32_t top[4] = {0, 0, 0, 0};
-            uint32_t samples = 0;
-            while (analyzer_analyze(anl, window)) {
-                samples += window;
-                int16_t tc = anl->topcount;
-                tally_counts[tc] += anl->counts[tc];
-                // printf("%10d [%3u]\t%10d [%3u]\t%10d [%3u]\n", anl->topcount, anl->counts[anl->topcount], anl->next, anl->counts[anl->next], anl->nextnext, anl->counts[anl->nextnext]);
+
+    playback_rewind(pb);
+
+    // determine "making sound" period and "not making sound" period; there should be 15 of them
+    // (and bonus also detect the bleeps)
+
+    int making_sound = 0;
+    int silent = 0;
+    uint32_t sample = 0;
+
+    /*
+     * sample       time    delta   event
+     * 64112        01.454          initial beep begin
+     * 69689        01.580  00.126  end
+     * 104486       02.369  00.915  large click begin (delta very close to 1 s from initial beep begin)
+     * 104688       02.374  00.005  end
+     * 108671       02.464  01.010  after-click begin (like a button coming back up after being pressed; very very close to 1s from in)
+     * 109400       02.481  00.017  end
+     * 118125       02.679          first transmission begin
+     * 170316       03.862  01.183  end (52191 samples)
+     * 174713       03.962  00.077  after?-click begin (delta is for time after first trans end)
+     * 175623       03.982  00.020  end
+     * 179369       04.067  00.182  after??-click begin (delta is fo rtime after first trans end)
+     * 180007       04.082  00.015  end
+     * 184266       04.178          second transmission begin
+     * 236186       05.356  01.178  end (51920 samples)
+     * 
+     */
+    #define SWITCH_THRESH   10
+    #define SOUND_THRESH    150
+    #define SILENCE_THRESH  100
+    while (playback_iterate(pb)) {
+        ++sample;
+        int16_t c = pb->chn[0];
+        if (abs(c) > SOUND_THRESH) {
+            ++making_sound;
+            if (making_sound == SWITCH_THRESH) {
+                printf("%-13u %s: making sound\n", sample, sample_timestr(sample, riff->fmt->sample_rate));
             }
-            uint32_t min = 999, max = 0;
-            for (size_t j = 0; j < 4; ++j) {
-                for (size_t i = 0; i < STEPS; ++i) {
-                    if (tally_counts[i] > tally_counts[top[j]] && (j == 0 || tally_counts[top[j-1]] > tally_counts[i])) {
-                        top[j] = i;
-                        if (min > i) min = i;
-                        if (max < i) max = i;
-                    }
-                }
+            if (making_sound >= SWITCH_THRESH && making_sound < SWITCH_THRESH + 10) {
+                printf("%-13u %s %u %d %d\n", sample, sample_timestr(sample, riff->fmt->sample_rate), abs(c), making_sound, silent);
             }
-            double score = (double)(tally_counts[top[0]] + tally_counts[top[1]] + tally_counts[top[2]] + tally_counts[top[3]]) * (max - min) / samples;
-            if (score > top_score) {
-                printf("new top score %16lf: window = %5zu, slide = %5zu, top = %4u, %4u, %4u, %4u [%5llu]\n", score, window, slide, top[0], top[1], top[2], top[3], tally_counts[top[0]] + tally_counts[top[1]] + tally_counts[top[2]] + tally_counts[top[3]]);
-                top_score = score;
+            if (making_sound >= SWITCH_THRESH && silent > 0) if (silent >= SWITCH_THRESH) silent = 0; else --silent;
+        } else if (abs(c) < SILENCE_THRESH) {
+            ++silent;
+            // if (silent < SWITCH_THRESH) {
+            //     printf("%s %u %d %d\n", sample_timestr(sample, riff->fmt->sample_rate), abs(c), making_sound, silent);
+            // }
+            if (silent == SWITCH_THRESH) {
+                printf("%-13u %s: silent\n", sample, sample_timestr(sample, riff->fmt->sample_rate));
             }
+            if (silent >= SWITCH_THRESH && making_sound > 0) if (making_sound >= SWITCH_THRESH) making_sound = 0; else --making_sound;
         }
     }
-    // if (riff->fmt->channels == 2) {
-    //     while (playback_iterate(pb)) {
-    //         printf("%10d %10d\n", pb->chn[0], pb->chn[1]);
+
+    // // we define quality of parameters as the set where we saw the highest top count divided by total counts,
+    // // i.e. a perfect score for 100 samples is where top sample count = 100
+    // // and a 50% score is where the top sample count = 50, so we can calculate the quality
+    // // of a single analyze as topcount / count,
+    // // and the aggregate score as the sum of all topcounts divided by the sum of counts
+    // // our goal is to find a signal with ON and OFF (true/false, 1/0) elements, which means we also want
+    // // to raise quality for when there are two distinct signals, where the distinction is defined
+    // // by the steps implementation (i.e. a 58 is considered distinctly different from a 59)
+    // // finally, the end quality is defined as the top TWO hits across the entire run, and their counts over the entire counts
+    // double top_score = 0;
+    // #define STEPS 1000
+    // struct analyzer_t* anl = analyzer_init(pb, min, max, STEPS);
+    // uint64_t tally_counts[STEPS];
+    // // for (size_t window = 30; window < 500; ++window) {
+    // //     double local_score = 0;
+    // //     size_t local_slide = 0;
+    // //     uint32_t local_top[4] = {0,0,0,0};
+    // //     uint64_t local_tally_sum = 0;
+    // //     for (size_t slide = 0; slide < window; ++slide) {
+    // //         printf("w = %5zu, slide = %5zu\r", window, slide);
+    // //         fflush(stdout);
+    //         playback_rewind(pb);
+    // //         // "slide"
+    // //         for (size_t i = 0; i < slide; ++i) playback_iterate(pb);
+    // //         memset(tally_counts, 0, sizeof(uint64_t) * STEPS);
+    // //         uint32_t top[4] = {0, 0, 0, 0};
+    // //         uint32_t samples = 0;
+    // //         while (analyzer_analyze(anl, window)) {
+    // //             samples += window;
+    // //             int16_t tc = anl->topcount;
+    // //             tally_counts[tc] += anl->counts[tc];
+    // //             // printf("%10d [%3u]\t%10d [%3u]\t%10d [%3u]\n", anl->topcount, anl->counts[anl->topcount], anl->next, anl->counts[anl->next], anl->nextnext, anl->counts[anl->nextnext]);
+    // //         }
+    // //         uint32_t min = 999, max = 0;
+    // //         for (size_t j = 0; j < 4; ++j) {
+    // //             for (size_t i = 0; i < STEPS; ++i) {
+    // //                 if (tally_counts[i] > tally_counts[top[j]] && (j == 0 || tally_counts[top[j-1]] > tally_counts[i])) {
+    // //                     top[j] = i;
+    // //                     if (min > i) min = i;
+    // //                     if (max < i) max = i;
+    // //                 }
+    // //             }
+    // //         }
+    // //         double score = (double)(tally_counts[top[0]] + tally_counts[top[1]] + tally_counts[top[2]] + tally_counts[top[3]]) * (max - min) / samples;
+    // //         if (score > top_score) {
+    // //             printf("new top score %16lf: window = %5zu, slide = %5zu, top = %4u, %4u, %4u, %4u [%5llu]\n", score, window, slide, top[0], top[1], top[2], top[3], tally_counts[top[0]] + tally_counts[top[1]] + tally_counts[top[2]] + tally_counts[top[3]]);
+    // //             local_score = top_score = score;
+    // //         }
+    // //         if (score > local_score) {
+    // //             local_score = score;
+    // //             local_slide = slide;
+    // //             memcpy(local_top, top, sizeof(uint32_t) * 4);
+    // //             local_tally_sum = tally_counts[top[0]] + tally_counts[top[1]] + tally_counts[top[2]] + tally_counts[top[3]];
+    // //         }
+    // //     }
+    // //     if (local_score < top_score) printf("  local score %16lf: window = %5zu, slide = %5zu, top = %4u, %4u, %4u, %4u [%5llu]\n", local_score, window, local_slide, local_top[0], local_top[1], local_top[2], local_top[3], local_tally_sum);
+    // // }
+    // uint32_t sample = 0;
+    // while (playback_iterate(pb)) {
+    //     sample++;
+    //     uint32_t hs = sample / 441;
+    //     uint32_t s = 0;
+    //     if (hs > 99) {
+    //         s = hs / 100;
+    //         hs = hs % 100;
     //     }
-    // } else {
-    //     while (playback_iterate(pb)) {
-    //         printf("%10d\n", pb->chn[0]);
-    //     }
+    //     printf("%02u.%02u %10d %10d\n", s, hs, pb->chn[0], pb->chn[1]);
     // }
 }
